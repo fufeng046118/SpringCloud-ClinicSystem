541034f6ee1147ac8bec0d98df52b4fb5140ccae
fufeng <2431372757@qq.com>
2020-05-28 22:21:48 +0800

    22:21

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM java:8
-
-COPY *.jar /app.jar
-
-CMD ["--server.port=7001"]
-
-EXPOSE 7001
-
-ENTRYPOINT ["java","-jar","/app.jar"]
--- Spring-Cloud-Config/src/main/java/cn/project/Eureka_Config_Server.java
+++ Spring-Cloud-Config/src/main/java/cn/project/Eureka_Config_Server.java
@@ -2,10 +2,12 @@ package cn.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Eureka_Config_Server {
     public static void main(String[] args) {
         SpringApplication.run(Eureka_Config_Server.class,args);
